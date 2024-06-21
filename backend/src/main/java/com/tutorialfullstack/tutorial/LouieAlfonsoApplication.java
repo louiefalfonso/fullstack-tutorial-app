af93291dd2ffa32569b1f59fafe706512aa4ffa2
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-21 22:10:27 +0100

    Initial commit

---
+++ backend/src/main/java/com/tutorialfullstack/tutorial/LouieAlfonsoApplication.java
@@ -0,0 +1,13 @@
+package com.tutorialfullstack.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouieAlfonsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouieAlfonsoApplication.class, args);
+	}
+
+}
